a25cde51547c7eccfa3a31d6ddd6afe1517b4a55
mohit2494 <mohit2494@gmail.com>
2019-06-18 05:29:49 +0530

    added python+sql

--- React - The Complete Guide/code/react-complete-guide/src/App.js
+++ React - The Complete Guide/code/react-complete-guide/src/App.js
@@ -6,9 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>
-          Hi, I'm a React app
-        </h1>
+		<h1>
+			Hi, I'm a React app
+		</h1>
       </div>
     );
   }
